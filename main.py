e6d4d343b7529496d001f23a6860fdcf17bf0fae
Abdulwahab Urinboev <abdulwahab@Abdulwahabs-MacBook-Pro.local>
2026-01-25 22:43:59 +0500

    Automate Github login using Selenium with evn credentials

--- main.py
+++ main.py
@@ -18,8 +18,12 @@ sign_in_button.click()
 username = os.environ.get("GITHUB_USERNAME")
 password = os.environ.get("GITHUB_PASSWORD")
 
+# credentials are export to local terminal to be secure
 username_input = browser.find_element(By.ID, 'login_field').send_keys(username)
 password_input = browser.find_element(By.ID, 'password').send_keys(password)
 
+# after entering credentials find the sign in button and click
+sign_in_button = browser.find_element(By.NAME, "commit")
+sign_in_button.click()
 
-input("Press Enter to close...")
+input("Press Enter after observing the result...")

8af556468b3f73426f83f7e5a3cef760598ae94e
Abdulwahab Urinboev <abdulwahab@Abdulwahabs-MacBook-Pro.local>
2026-01-29 09:51:31 +0500

    Add ReposPage skeleton

---
+++ pages/repos_page.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.common.by import By
+from .base_page import BasePage
+
+
+class ReposPage(BasePage):
+    def is_open(self):
+        pass
+
+    def get_repo_names(self):
+        pass
+
+    def get_repo_count(self):
+        pass
